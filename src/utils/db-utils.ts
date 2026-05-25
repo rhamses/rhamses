ef98faf56926c208b2be8e8877f02134bc478a45
Rhamses <rhamses.soares@gmail.com>
2026-05-25 15:33:24 -0300

    adding prefix on the tables

--- src/utils/db-utils.ts
+++ src/utils/db-utils.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 import type { Database } from "./types/database.ts";
+import { stripTablePrefix } from "../db/table-prefix.ts";
 import {
   TABLE_PREFIX,
   EDP_LOGICAL_TABLES,
